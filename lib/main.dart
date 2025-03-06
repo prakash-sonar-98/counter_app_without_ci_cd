91457bdc89dfa85f7b45cc0d2d49842b01d78e91
Prakash Sonar <prakashsonar@JustMacs-MacBook-Air.local>
2025-03-06 19:45:17 +0530

    Appbar color changes

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,8 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
         title: Text(widget.title),
       ),
       body: Center(
